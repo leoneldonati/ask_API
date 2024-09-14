a3a9da7878ac5f65824a87eecc010723d71446e1
Leonel Donati <leonelroman321@gmail.com>
2024-09-14 10:11:04 -0300

    inserted some changes on addPost fn

--- src/controllers/posts.ts
+++ src/controllers/posts.ts
@@ -1,15 +1,24 @@
 import { postDb } from "@models/post";
-import { parse } from "@formkit/tempo"
+import { parse } from "@formkit/tempo";
 import type { Response } from "express";
 import { checkLinksOnContent, checkUsersMentioned } from "@utils/post";
+import { uploadFiles } from "@services/cloudinary";
 
-export async function addPost(req: ExtendedReqWithToken, res: Response): Promise<Response> {
+export async function addPost(
+  req: ExtendedReqWithToken,
+  res: Response
+): Promise<Response> {
   const postPayload = req.body;
   const decodedToken = req.decodedToken;
-  
-  const links = checkLinksOnContent(postPayload?.content)
-  const usersMentioned = checkUsersMentioned(postPayload?.content)
+  const files = req.files;
+
+  // CHEQUEAR EL CONTENIDO
+  //const links = checkLinksOnContent(postPayload?.content);
+  //const usersMentioned = checkUsersMentioned(postPayload?.content);
+
+  // SUBIR ARCHIVOS
+  //const uploadedFiles = files !== null ? await uploadFiles(Object.values(files as any), { folder: "posts" }) : null;
 
   
-  return res.json({ postPayload, decodedToken });
+  return res.json({});
 }
