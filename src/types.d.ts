5a6ab59ed937cf52e0b64a8b3caf3759290f9c47
Leonel Donati <leonelroman321@gmail.com>
2024-09-14 10:08:49 -0300

    added some types

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,8 +1,28 @@
 import type { Request } from "express";
 
+export interface RawFile {
+  name: string;
+  data: Buffer;
+  size: number;
+  encoding: string;
+  tempFilePath: string;
+  truncated: boolean;
+  mimetype: string;
+  md5: string;
+}
+
 interface Image {
   secureUrl: string;
   publicId: string;
+  width: number;
+  height: number;
+}
+interface Location {
+  city: string;
+  country: string;
+}
+interface Settings {
+  colorScheme: 'dark' | 'light';
 }
 declare global {
   interface ExtendedReqWithToken extends Request {
@@ -19,5 +39,25 @@ declare global {
     comments: Array;
     links: Array<string> | null;
     usersMentioned: Array<string> | null;
+  }
+
+  interface UserWithOutId {
+    name: string;
+    username: string;
+    bio: string;
+    media: {
+      avatar: Image | null;
+      frontPage: Image | null;
+    };
+    dateOfBirth: Date;
+    email: string;
+    hash: string;
+    isVerified: boolean;
+    location: Location;
+    settings: Settings;
+    createdAt: Date;
+    updatedAt: Date;
+    followers: Array<string>;
+    followed: Array<string>;
   }
 }
