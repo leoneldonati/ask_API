8a314487bada54b24ee10d3399fa2bba80495d90
Leonel Donati <leonelroman321@gmail.com>
2024-09-14 10:09:34 -0300

    created env variables

--- src/config/env-variables.ts
+++ src/config/env-variables.ts
@@ -8,3 +8,7 @@ export const DB_PASS = process.env.DB_PASS
 export const DB_USERNAME = process.env.DB_USERNAME
 
 export const SECRET_JWT = process.env.SECRET_JWT
+
+export const CLD_NAME= process.env.CLD_NAME
+export const CLD_KEY = process.env.API_KEY
+export const CLD_SECRET = process.env.API_SECRET
